bfac992cc880917ef278fc278301bbfed10703de
Walter de Valk <walterdevalk@gmail.com>
2014-06-11 23:13:46 +0200

    first attemt to the asignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,15 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+
+makeCacheMatrix <- function(x = matrix()) {
+  # matrix() --> list()
+  # reads a matrix and return a list with the matrix and it's inverted matrix
+  # assuming the matrix can be inverted
+  inverse <- solve( x )
+  list( x, inverse=inverse) 
 }
 
 
@@ -12,4 +19,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if( "inverse" %in% names(x) ){
+          message("getting cached data")
+          return( inversecache$inverse )
+        }
+        
+        solve( x ) 
 }
+
+
+x <- matrix( 1:4, nrow=2)
+inversecache <- makeCacheMatrix( x )
+
+
+cacheSolve( inversecache)
+cacheSolve( x )
+
+
