ee3e3ea299b6dbc39ec2d38a785b90b4135fa740
walterdevalk <walterdevalk@gmail.com>
2014-06-12 18:23:30 +0200

    blaat

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
 
 
-
-
+# reads a matrix and return a list with the matrix and it's inverted matrix
+# assuming the matrix can be inverted
+ 
 makeCacheMatrix <- function(x = matrix()) {
   # matrix() --> list()
-  # reads a matrix and return a list with the matrix and it's inverted matrix
-  # assuming the matrix can be inverted
-  inverse <- solve( x )
+ inverse <- solve( x )
   list( x, inverse=inverse) 
 }
 
 
-## Write a short comment describing this function
+# reads the cachedMatrix
+# If there is an inversed matrix present; return that one
+# Otherwise create an inverted matrix based on x
+# Returns: inversed matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -30,6 +31,7 @@ cacheSolve <- function(x, ...) {
 }
 
 
+# test
 x <- matrix( 1:4, nrow=2)
 inversecache <- makeCacheMatrix( x )
 
